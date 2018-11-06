e73631aa2c12fd5cd0daa5c9f39eb97ae3094280
Steve Love @ home <steve@asquith>
2018-11-06 11:08:40 +0000

    Adding combination key dict

--- src/pyning/combinationdict.py
+++ src/pyning/combinationdict.py
@@ -1,7 +1,37 @@
 from collections import UserDict, Mapping
+import re
 
 
 class CombinationDict( UserDict ):
-    def __init__( self, separator, content ):
-        pass
+    def __init__( self, separator, content=None, **kwargs ):
+        self.separator = separator
+        self.key_pattern = re.compile( rf'(?<!\\){re.escape(self.separator)}' )
+        UserDict.__init__( self, content, **kwargs )
 
+    def _locate( self, key ):
+        res = self.data
+        for k in key:
+            if k not in res:
+                return None
+            res = res[ k ]
+        return res
+
+    def update( *args, **kwargs ):
+        self, other, *args = args
+        for k, v in other.items():
+            if k in self.data and isinstance( v, Mapping ):
+                self[ k ].update( v )
+            else:
+                self[ k ] = v
+        return self
+
+    def __getitem__( self, key ):
+        return self._locate( self.key_pattern.split( key ) )
+
+    def __setitem__( self, key, value ):
+        key = self.key_pattern.split( key )
+        res = self._locate( key[ :-1 ] )
+        res[ key[ -1 ] ] = value
+
+    def __contains__( self, key ):
+        return self._locate( self.key_pattern.split( key ) )
--- src/tests/test_pyning/test_combinationdict.py
+++ src/tests/test_pyning/test_combinationdict.py
@@ -6,5 +6,58 @@ def test_key_at_root_is_located():
     assert items[ 'a' ] == 10
 
 
+def test_key_nested_1_level_is_located():
+    items = CombinationDict( '/', { 'a': { 'b': 10 } } )
+    assert items[ 'a/b' ] == 10
+
+
+def test_escaped_separator_is_used_as_direct_key():
+    items = CombinationDict( '.', { 'a': { 'b\\.c': { 'd': 10 } } } )
+    assert items[ 'a.b\\.c.d' ] == 10
+
+
+def test_nested_value_can_be_updated():
+    items = CombinationDict( '.', { 'a': { 'b': { 'c': 10 } } } )
+    items[ 'a.b.c' ] = 100
+    assert items[ 'a' ][ 'b' ][ 'c' ] == 100
+
+
+def test_item_value_can_be_a_list():
+    items = CombinationDict( '.', { 'a': [ 1, 2 ] } )
+    assert items[ 'a' ][ 0 ] == 1
+
+
+def test_nested_item_can_be_a_list():
+    items = CombinationDict( '.', { 'a': { 'b': [ 1, 2 ] } } )
+    assert items[ 'a.b' ][ 0 ] == 1
+
+
+def test_nested_dict_can_be_updated():
+    items = CombinationDict( '.', { 'a': { 'b': 10, 'c': 20 } } )
+    items.update( { 'a': { 'b': 100 } } )
+    assert items[ 'a.b' ] == 100
+    assert items[ 'a.c' ] == 20
+
+
+def test_nested_dict_can_be_updated_from_tuple():
+    items = CombinationDict( '.', { 'a': { 'b': 10 } } )
+    items[ 'a' ].update( [ ( 'c', 100 ), ( 'e', 1 ) ] )
+    assert items[ 'a.c' ] == 100
+    assert items[ 'a.e' ] == 1
+
+
+def test_update_respects_nesting_notation():
+    items = CombinationDict( '.', { 'a': { 'b': 10 } } )
+    items.update( { 'a.b': 100 } )
+    assert items[ 'a' ][ 'b' ] == items[ 'a.b' ]
+    assert 'a.b' not in set( items.keys() )
+
+
+def test_separator_for_nesting_can_be_escaped():
+    items = CombinationDict( '.', { 'a': { 'b': 10 } } )
+    items[ r'a\.b' ] = 100
+    assert items[ 'a.b' ] == 10
+    
+
 if __name__ == '__main__':
     pass
